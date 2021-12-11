7972b69c4ea7f348ed17c2be257d0eb5e6119ed8
Jofay-zs <jofayzs19@gmail.com>
2021-12-11 01:22:50 -0500

    Subiendo el diseño final de usuarios y agregando un filtro

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   });
 
   return (
-    <div className="overflow-x-hidden">
+    <div className="">
       <ApolloProvider client={client}>
         <BrowserRouter>
           <Routes>
--- src/pages/users/UsersIndex.jsx
+++ src/pages/users/UsersIndex.jsx
@@ -1,70 +1,126 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { GET_USERS } from "../../graphql/users/usersQueries";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../../styles/tableStyles.css";
 
 const UsersIndex = () => {
-  return (
-    <main>
-      <section>
-        <h2 className="font-bold text-3xl">Users</h2>
-      </section>
-      <section>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
-        sapiente ab dolorum commodi ad, nisi consectetur provident iure.
-        Molestias, dolor.
-      </section>
-      <hr />
-      <section className="sm:hidden">
-        <PhoneTable />
-      </section>
-      <section className="hidden lg:block">
-        <LargeTable/>
-      </section>
-    </main>
-  );
-};
-
-const PhoneTable = () => {
   const { data, error, loading } = useQuery(GET_USERS);
-  return <div>Phone Table</div>;
-};
+  const [search, setSearch] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState();
 
-const LargeTable = () => {
-  const { data, error, loading } = useQuery(GET_USERS);
+  useEffect(async () => {
+    if (data) {
+      await setFilteredUsers(
+        data.findAllUsers.filter((e) => {
+          return JSON.stringify(e).toLowerCase().includes(search.toLowerCase());
+        })
+      );
+    }
+  }, [search, data]);
+
   return (
-    <div className="w-screen h-full flex justify-center items-center">
-      <table className="default-table w-3/5 h-auto">
-        <thead>
-          <tr>
-            <th>Identification</th>
-            <th>Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Country</th>
-            <th>Rol</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.findAllUsers.map((u) => {
-              return (
-                <tr key={u._id}>
-                  <td>{u.identification}</td>
-                  <td>{u.userName}</td>
-                  <td>{u.userLastName}</td>
-                  <td>{u.email}</td>
-                  <td>{u.country}</td>
-                  <td>{u.rol}</td>
-                  <td>{u.status}</td>
+    <main className="w-full h-auto">
+      <div className="">
+        <section className="w-full flex justify-center items-center">
+          <h2 className="font-bold text-3xl">Usuarios</h2>
+        </section>
+        <section className="flex flex-col justify-center items-center mb-2 w-full px-10 sm:grid sm:gap-5 sm:grid-cols-2">
+          <div className="border-gray-900 h-auto w-full">
+            <input
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+              type="text"
+              placeholder={"🔎"}
+              className="w-full my-2 bg-gray-100 border border-gray-400 rounded-md outline-none focus:border-color1 py-2 px-1"
+            />
+          </div>
+          <div className="flex justify-around bg-color1 rounded-md text-gray-100 px-1 py-2 w-full">
+            <div className="">Todos</div>
+            <div className="">Estudiantes</div>
+            <div className="">Lideres</div>
+          </div>
+        </section>
+        <hr />
+        <section className="lg:hidden mt-2">
+          <div className="px-3 grid sm:grid-cols-2 sm:gap-2 md:grid-cols-3">
+            {filteredUsers &&
+              filteredUsers.map((u) => {
+                return (
+                  <div
+                    key={u._id}
+                    className="border-2 border-color1 rounded-sm px-2 py-3 mb-2 shadow"
+                  >
+                    <div>
+                      <b>Nombre:</b>
+                      {u.userName}
+                    </div>
+                    <div>
+                      <b>Apellido:</b>
+                      {u.userLastName}
+                    </div>
+                    <div>
+                      <b>Identificacion:</b>
+                      {u.identification}
+                    </div>
+                    <div>
+                      <b>Correo:</b>
+                      {u.email}
+                    </div>
+                    <div>
+                      <b>Pais:</b>
+                      {u.country}
+                    </div>
+                    <div>
+                      <b>Rol:</b>
+                      {u.rol}
+                    </div>
+                    <div>
+                      <b>Estado:</b>
+                      {u.status}
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </section>
+        <section className="hidden lg:block mt-2">
+          <div className="w-screen h-full flex justify-center items-center">
+            <table className="default-table w-3/5 h-auto">
+              <thead>
+                <tr>
+                  <th>Identification</th>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Correo</th>
+                  <th>Pais</th>
+                  <th>Rol</th>
+                  <th>Estado</th>
                 </tr>
-              );
-            })}
-        </tbody>
-      </table>
-    </div>
+              </thead>
+              <tbody>
+                {filteredUsers &&
+                  filteredUsers.map((u) => {
+                    return (
+                      <tr key={u._id}>
+                        <td>{u.identification}</td>
+                        <td>{u.userName}</td>
+                        <td>{u.userLastName}</td>
+                        <td>{u.email}</td>
+                        <td>{u.country}</td>
+                        <td>{u.rol}</td>
+                        <td>{u.status}</td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 };
 
